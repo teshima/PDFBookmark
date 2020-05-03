4c7d9d636f02b969a53a8496ef0e2e90dda2d3e6
Y.Teshima <yteshima@gmail.com>
2020-05-03 21:24:10 +0900

    Correctly run

--- PDFBookmark.py
+++ PDFBookmark.py
@@ -14,46 +14,53 @@ parser.add_argument('input_pdf_filename', help='input PDF filename', \
                     type=argparse.FileType('rb') )
 parser.add_argument('output_pdf_filename', help='output pdf filename', \
                     type=argparse.FileType('wb'))
-parser.add_argument('-p', '--page_bias',  help='page bias', type=int )
+parser.add_argument('-p', '--page_bias',  help='page bias', type=int, default=0 )
 args = parser.parse_args()
 
+pdfReader = PdfFileReader( args.input_pdf_filename )
 pdfWriter = PdfFileWriter()
-pdfWriter.cloneDocumentFromReader( PdfFileReader( args.input_pdf_filename ) )
+pdfWriter.appendPagesFromReader( pdfReader )
 numOfPages = pdfWriter.getNumPages()
 
-bookmarkCSV = csv.reader( args.bookmark_txt, delimiter='¥t' )
+bookmarkCSV = csv.reader( args.bookmark_txt, delimiter='\t' )
 
 upper_parents = [  ]
 prev_bookmark = None
 current_parent = None
+page_bias = args.page_bias
 
 for row in bookmarkCSV :
-    pageNumber = row[ 1 ]
+    pageNumber = int( row[ 0 ] ) + page_bias
 
     if len(row) < 2 :
         # Doesn't fullfill bookmark format
-        print("Invalid bookmark text : ",bookmarkCSV.line_num, ':', row )
+        sys.stdout.write("Invalid bookmark text : " + str( bookmarkCSV.line_num) )
+        continue
     if numOfPages < pageNumber :
         # Over PDF pages
-        print( "Specified page number in bookmark file is over PDF pages count : ",bookmarkCSV.line_num )
+        sys.stdout.write("Specified page number in bookmark file is over PDF pages count in line " + str( bookmarkCSV.line_num) )
+        sys.exit(1)
     # Count depth
     depth = len( row ) - 2
     text = row[ - 1 ]
 
-    if depth == len( upper_parents ) - 1 :
+    if depth < len( upper_parents ) :
         # Upper depth
         if len( upper_parents ) == 0 :
             # Cannot go upper more
-            print("Cannot go lower more : ",bookmarkCSV.line_num )
-        current_parent = upper_parents.pop()
+            sys.stdout.write("Cannot go lower more : " + str( bookmarkCSV.line_num) )
+            sys.exit(1)
+        current_parent = upper_parents[ depth ]
+        del upper_parents[ depth: ]
     elif depth == len( upper_parents ) + 1 :
         # lower depth
         upper_parents.append(current_parent)
         current_parent = prev_bookmark
-    elif depth < len( upper_parents ) - 1 or len( upper_parents ) + 1 < depth :
-        print("Not continuous depth : ", bookmarkCSV.line_num)
+    elif len( upper_parents ) + 1 < depth :
+        sys.stdout.write("Not continuous depth : " + str( bookmarkCSV.line_num) )
+        sys.exit(1)
 
-    prev_bookmark = pdfWriter.addBookmark(text, pageNumber, parent = current_parent )
+    prev_bookmark = pdfWriter.addBookmark(text, pageNumber - 1, parent = current_parent )
 
 pdfWriter.write( args.output_pdf_filename )
 
